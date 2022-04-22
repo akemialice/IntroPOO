bc0dfd47b72f83cdc87ddba93fc4d662c9ac586d
Akemi Alice <86382666+akemialice@users.noreply.github.com>
2022-04-22 11:45:56 -0300

    Update Quadrado.cs

--- SP304484X/IntroPOO/Quadrado.cs
+++ SP304484X/IntroPOO/Quadrado.cs
@@ -1,12 +1,7 @@
 public class Quadrado
 {
-    //propriedade
     public double Lado {get; set;}
-
-     public double Area {get => Lado*Lado;}
-  
-     public double Perimetro  {get => 4*Lado;}
-
-    //construtor
+    public double Area {get => Lado*Lado;}
+    public double Perimetro  {get => 4*Lado;}
     public Quadrado(double lado) => this.Lado = lado; 
 }
